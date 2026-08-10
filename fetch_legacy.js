9166550c544f61f9129eaa1625092a58b723acd6
Badr Alaa <balaa6538@gmail.com>
2026-08-10 23:34:39 +0300

    refactor: remove legacy migration scripts and testing utilities

--- fetch_legacy.js
+++
@@ -1,2 +0,0 @@
-const url = 'https://script.google.com/macros/s/AKfycbx_v0B_D45CjH-C9B8O-7A4Hwz12hHj2P0vXF3yFk7gL0P2D2MvR8XG/exec?action=getSettings';
-// Wait, the URL in app.js is different. Let me read GOOGLE_SHEETS_URL from app.js.
